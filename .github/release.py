b3eb40f99ee14ecad9d56f8fe6459df9f0765a57
Maxim Kurbatov <maksimkurb@gmail.com>
2025-10-20 00:43:37 +0500

    Trigger gh action

--- .github/release.py
+++ .github/release.py
@@ -18,8 +18,8 @@ def _goarch(goos):
     yield 'amd64'
     yield 'arm'
     yield 'arm64'
-    yield 'mips'
     yield 'mipsel'
+    yield 'mips'
     if goos == 'linux':
         yield 'ppc64le'
         yield 's390x'
